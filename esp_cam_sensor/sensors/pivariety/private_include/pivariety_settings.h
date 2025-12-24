3b22e87b5f87410d83876c8318467844f4e5ded1
lvbin <stephen@arducam.com>
2025-12-24 18:32:37 +0800

    add multi resolution

--- esp_cam_sensor/sensors/pivariety/private_include/pivariety_settings.h
+++ esp_cam_sensor/sensors/pivariety/private_include/pivariety_settings.h
@@ -26,28 +26,22 @@ static const pivariety_reginfo_t pivariety_mipi_stream_off[] = {
     {PIVARIETY_REG_END,       0x00000000},
 };
 
-static const pivariety_reginfo_t pivariety_MIPI_2lane_raw10_640x480_30fps[] = {
-    {PIXFORMAT_INDEX_REG,  0x00000000},
-    {RESOLUTION_INDEX_REG, 0x00000003},
-    {STREAM_ON,            0x00000001},
-    {PIVARIETY_REG_END,    0x00000000},
-};
 
-static const pivariety_reginfo_t pivariety_MIPI_2lane_raw10_1024x600_30fps[] = {
+static const pivariety_reginfo_t pivariety_MIPI_2lane_raw10_1024x600_60fps[] = {
     {PIXFORMAT_INDEX_REG,  0x00000000},
     {RESOLUTION_INDEX_REG, 0x00000002},
     {STREAM_ON,            0x00000001},
     {PIVARIETY_REG_END,    0x00000000},
 };
 
-static const pivariety_reginfo_t pivariety_MIPI_2lane_raw10_1280x720_30fps[] = {
+static const pivariety_reginfo_t pivariety_MIPI_2lane_raw10_1280x720_60fps[] = {
     {PIXFORMAT_INDEX_REG,  0x00000000},
     {RESOLUTION_INDEX_REG, 0x00000001},
     {STREAM_ON,            0x00000001},
     {PIVARIETY_REG_END,    0x00000000},
 };
 
-static const pivariety_reginfo_t pivariety_MIPI_2lane_raw10_1600x1200_30fps[] = {
+static const pivariety_reginfo_t pivariety_MIPI_2lane_raw10_1920x1080_30fps[] = {
     {PIXFORMAT_INDEX_REG,  0x00000000},
     {RESOLUTION_INDEX_REG, 0x00000000},
     {STREAM_ON,            0x00000001},
